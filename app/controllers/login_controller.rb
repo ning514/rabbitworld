412bf8c318c1a8092c699af89cbf1639b1cf4c22
DESKTOP-0GQQHRG\uu694 <uu694a@gmail.com>
2022-02-21 21:31:06 +0800

    add postgresql

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,3 @@
-require 'net/http'
-require 'uri'
-require 'json'
 class LoginController < ApplicationController
     def index
     end
@@ -12,6 +9,11 @@ class LoginController < ApplicationController
     end
 
     def create
+        ::User.create({
+            username: params[:new_acc][:username],
+            email: params[:new_acc][:email],
+            psw: params[:new_acc][:psw]
+        })
     end
 end
   
